a94e8fcb5948b3054b48b5c5c1754f73f46dca0b
Alaa Elias <alaaelias@gmail.com>
2016-04-14 23:39:41 -0700

    Cleaning Up

--- app/src/main/java/com/example/alaaelias/portfolio/MainActivity.java
+++ app/src/main/java/com/example/alaaelias/portfolio/MainActivity.java
@@ -1,36 +1,17 @@
 package com.example.alaaelias.portfolio;
 
 import android.os.Bundle;
-import android.os.Handler;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import java.util.Timer;
-
-
-import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
     }
 
     @Override
@@ -57,20 +38,8 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onStart() {
-        Handler mHandler = new Handler();
-
-
-        Timer myTimer = new Timer();
-        MyTimerTask myTimerTask= new MyTimerTask();
-        myTimer.scheduleAtFixedRate(myTimerTask, 0, 3000);
         super.onStart();
     }
 
-    private class MyTimerTask extends TimerTask {
-        @Override
-        public void run() {
-            //get and send location information
-            Log.v("profelio", "Running...");
-        }
-    }
+
 }
